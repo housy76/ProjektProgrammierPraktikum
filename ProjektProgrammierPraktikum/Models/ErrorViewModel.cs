80682118c5b3f03f02a5129a6ec9ea287d824d68
Housy <housy76@live.de>
2017-10-09 14:08:03 +0200

    Add project files.

---
+++ ProjektProgrammierPraktikum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjektProgrammierPraktikum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
