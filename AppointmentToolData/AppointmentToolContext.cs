fa261e5c3b3a950319ecc161a62e2cf38f527c34
gump.schrimps <max-freiberger@gmx.de>
2017-10-27 19:34:27 +0200

    neues Projekt für c#-Datenklassen angelegt, wie im Tutorial https://www.youtube.com/watch?v=WTVcLFTgDqs +NuGet Packages

---
+++ AppointmentToolData/AppointmentToolContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AppointmentToolData
+{
+    public class AppointmentToolContext : DbContext
+    {
+        //Konstruktor:
+        public AppointmentToolContext(DbContextOptions options) : base(options) { }
+        public DbSet<AppointmentSurvey> AppointmentSurveys { get; set; }
+
+    }
+}
---
+++ AppointmentToolData/Models/AppointmentSurvey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppointmentToolData.Models
+{
+    public class AppointmentSurvey
+    {
+        public int id { set; get; }
+        //public User creater { set; get; }
+        //public User[] members { set; get; }
+        //public Appointment[] Appointments { get; set; }
+
+    }
+}
