4b6a3b4d539d1a4270736b516476fd312465e167
K06 <max-freiberger@gmx.de>
2017-12-03 16:18:51 +0100

    Bugfix: UseUrls() funktioniert nur mit "http", nicht mit "https". Ports <1024 koennen nur mit rootrechten benutzt erwerden, die Anwendung soll nicht von root ausgefuehrt werden => vorlaeufig Port 8080. Der geloeschte Kommentar: Lasst uns lieber Blockkommentare schreiben.

--- TerminUndRaumplanung/Program.cs
+++ TerminUndRaumplanung/Program.cs
@@ -20,9 +20,7 @@ namespace TerminUndRaumplanung
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-            
-            //UseUrls is necessary for setting the web port for a fixed port to reach the application
-                .UseUrls("https://*:80")
+                .UseUrls("http://*:8080")
                 .Build();
     }
 }
