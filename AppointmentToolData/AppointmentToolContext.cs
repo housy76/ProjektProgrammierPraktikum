860b5eb49cfae6081cfe51c69c91cbdcbc85c5fe
Housy <housy76@live.de>
2017-12-04 15:44:49 +0100

    Revert "Unnuetze Verzeichnisse von der VM geloescht."
    This reverts commit 4418e653c376d874e56a240746fdf408b7db5052.

---
+++ AppointmentToolData/AppointmentToolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using AppointmentToolData.Models;
+
+namespace AppointmentToolData
+{
+    public class AppointmentToolContext : DbContext
+    {
+        //Konstruktor:
+        public AppointmentToolContext(DbContextOptions options) : base(options) { }
+        public DbSet<AppointmentSurvey> AppointmentSurveys { get; set; }
+
+    }
+}
