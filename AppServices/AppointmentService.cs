f06059add3cd1dc86371c8f05ee69eede71bfea0
Housy <housy76@live.de>
2017-12-04 17:44:58 +0100

    Created library project for all the services we build / use in our application
    This library project will include all the services we need in our application. All functions, what and how they will do something when we call tem on our classes / objects are defined here.

---
+++ AppServices/AppointmentService.cs
@@ -0,0 +1,86 @@
+﻿using AppData;
+using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppServices
+{
+    public class AppointmentService : IAppointment
+    {
+        public AppointmentContext _context;
+
+        public AppointmentService(AppointmentContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Appointment newAppointment, int surveyId)
+        {
+            _context.Add(newAppointment);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Appointment> GetAll()
+        {
+            return _context.Appointments
+                .Include(a => a.StartTime);
+        }
+
+        public Appointment GetById(int id)
+        {
+            return _context.Appointments
+                .Include(a => a.StartTime)
+                .Include(a => a.EndTime)
+                .Include(a => a.Room)
+                .Include(a => a.Ressources)
+                .FirstOrDefault(a => a.Id == id);
+        }
+
+        public DateTime GetEndTime(int id)
+        {
+            return
+                GetAll()
+                .FirstOrDefault(a => a.Id == id)
+                .EndTime;
+        }
+
+        public string GetRessources(int id)
+        {
+            return
+                GetAll()
+                .FirstOrDefault(a => a.Id == id)
+                .Ressources;
+        }
+
+        public string GetRoom(int id)
+        {
+            return
+                GetAll()
+                .FirstOrDefault(a => a.Id == id)
+                .Room;
+        }
+
+        public DateTime GetStartTime(int id)
+        {
+            return
+                GetAll()
+                .FirstOrDefault(a => a.Id == id)
+                .StartTime;
+        }
+
+        public AppointmentSurvey GetSurvey(int id)
+        {
+            //return
+            //    GetAll()
+            //    .FirstOrDefault(a => a.Id == id)
+            //    .SurveyId;
+
+            return _context
+                .AppointmentSurveys
+                .FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
---
+++ AppServices/AppointmentSurveyService.cs
@@ -0,0 +1,72 @@
+﻿using AppData;
+using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppServices
+{
+    public class AppointmentSurveyService : IAppointmentSurvey
+    {
+        public AppointmentContext _context;
+
+        public AppointmentSurveyService(AppointmentContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(AppointmentSurvey newSurvey)
+        {
+            _context.Add(newSurvey);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<AppointmentSurvey> GetAll()
+        {
+            return _context.AppointmentSurveys
+                            .Include(a => a.Subject);
+        }
+
+        public IEnumerable<Appointment> GetAppointments(int surveyId)
+        {
+            return _context.AppointmentSurveys
+                            .Include(s => s.Appointments)
+                            .FirstOrDefault(a => a.Id == surveyId)
+                            .Appointments;
+        }
+
+        public AppointmentSurvey GetById(int id)
+        {
+            return _context.AppointmentSurveys
+                            .Include(a => a.Subject)
+                                  .Include(a => a.Creator)
+                            .FirstOrDefault(a => a.Id == id);
+        }
+
+        public string GetCreator(int id)
+        {
+            return
+                            GetAll()
+                            .FirstOrDefault(a => a.Id == id)
+                            .Creator;
+        }
+
+        public string GetMembers(int id)
+        {
+            return
+                GetAll()
+                .FirstOrDefault(a => a.Id == id)
+                .Members;
+        }
+
+        public string GetSubject(int id)
+        {
+            return
+                           GetAll()
+                           .FirstOrDefault(a => a.Id == id)
+                           .Subject;
+        }
+    }
+}
