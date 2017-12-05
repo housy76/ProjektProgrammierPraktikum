76533eb9c25ded1a287fde2df22dfe5edbb95b05
Housy <housy76@live.de>
2017-12-05 10:29:14 +0100

    Created necessary models for views
    Every view can only receive one view. If you need data from different models at the same time in the same view, these models have to be build to store data from different system/application models.

---
+++ TerminUndRaumplanung/Models/AppointmentAddModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AppData.Models;
+
+namespace TerminUndRaumplanung.Models
+{
+    internal class AppointmentAddModel
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Room { get; set; }
+        public string Ressources { get; set; }
+        public AppointmentSurvey Survey { get; set; }
+    }
+}
---
+++ TerminUndRaumplanung/Models/SurveyDetailModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using AppData.Models;
+
+namespace TerminUndRaumplanung.Models
+{
+    public class SurveyDetailModel
+    {
+        public int SurveyId { get; set; }
+        public string Subject { get; set; }
+        public string Creator { get; set; }
+        public string Members { get; set; }
+        public IEnumerable<Appointment> Appointments { get; set; }
+    }
+}
