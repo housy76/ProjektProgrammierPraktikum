e6514eb9d0f7b166e2036a5ca3fa83bcc35c9322
Housy <housy76@live.de>
2017-12-07 15:08:55 +0100

    Revert "Klassen aus MainProjekt in AppData und AppServices verschoben"
    This reverts commit f85943256f0753f20d9fd26ad34e4722047fcf65.

--- TerminUndRaumplanung/Services/IEmailSender.cs
+++ TerminUndRaumplanung/Services/IEmailSender.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AppData
+namespace TerminUndRaumplanung.Services
 {
     public interface IEmailSender
     {
