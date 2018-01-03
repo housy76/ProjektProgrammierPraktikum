c35cb0d2debe4632b63af6569fe5a7203fb6fa4f
Housy <housy76@live.de>
2018-01-03 16:19:01 +0100

    Tried to create user roles and a adminuser on startup
    I tried several solutions but none worked completeley.
    At the moment the first Account ever will be created as a Admin Account. All ohter accounts will be normal User accounts
    The actual solution can be activated by uncomenting line 86 in Startup.cs
    The source to this solution is: https://stackoverflow.com/questions/45805561/asp-net-core-2-identity-di-errors-with-custom-roles

--- TerminUndRaumplanung/Startup.cs
+++ TerminUndRaumplanung/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using AppData;
 using AppServices;
 using AppData.Models;
+using System.Threading.Tasks;
+using System;
 
 namespace TerminUndRaumplanung
 {
@@ -33,7 +35,6 @@ namespace TerminUndRaumplanung
             services.AddTransient<IEmailSender, EmailSender>();
             services.AddTransient<IRessource, RessourceService>();
             services.AddTransient<IRoom, RoomService>();
-
 
             services.AddDbContext<AppointmentContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -62,12 +63,82 @@ namespace TerminUndRaumplanung
 
             app.UseAuthentication();
 
+
+            //create roles for users
+            //CreateRoles(app.ApplicationServices).Wait();
+
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+
+            IServiceScopeFactory scopeFactory = serviceProvider.GetRequiredService<IServiceScopeFactory>();
+
+            using (IServiceScope scope = scopeFactory.CreateScope())
+            {
+                RoleManager<IdentityRole> roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                string[] roleNames = { "Admin", "Manager", "Member" };
+                IdentityResult roleResult;
+
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        //create the roles and seed them to the database: Question 1
+                        roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+            }
+
+
+            //initializing custom roles 
+            //var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            //string[] roleNames = { "Admin", "Manager", "Member" };
+            //IdentityResult roleResult;
+
+            //foreach (var roleName in roleNames)
+            //{
+            //    var roleExist = await RoleManager.RoleExistsAsync(roleName);
+            //    if (!roleExist)
+            //    {
+            //        //create the roles and seed them to the database: Question 1
+            //        roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+            //    }
+            //}
+
+            //Here you could create a super user who will maintain the web app
+            var poweruser = new ApplicationUser
+            {
+
+                UserName = Configuration["AppSettings:UserName"],
+                Email = Configuration["AppSettings:UserEmail"],
+            };
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = Configuration["AppSettings:UserPassword"];
+            var _user = await UserManager.FindByEmailAsync(Configuration["AppSettings:AdminUserEmail"]);
+
+            if (_user == null)
+            {
+                var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+                if (createPowerUser.Succeeded)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+                }
+            }
         }
     }
 }
