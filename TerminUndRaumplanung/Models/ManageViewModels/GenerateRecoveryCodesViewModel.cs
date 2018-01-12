d08cf3d4274c5b0289ee9cd287b16dc4bee2adbf
Housy <housy76@live.de>
2018-01-12 18:23:55 +0100

    removed unnecessary usings

--- TerminUndRaumplanung/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
+++ TerminUndRaumplanung/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TerminUndRaumplanung.Models.ManageViewModels
+﻿namespace TerminUndRaumplanung.Models.ManageViewModels
 {
     /// <summary>
     /// 
